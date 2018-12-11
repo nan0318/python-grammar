39c6c5488baa8751946b0ada182394b84b68cd66
nan0318 <41815299+nan0318@users.noreply.github.com>
2018-12-11 12:27:13 +1100

    Update print.py

--- print.py
+++ print.py
@@ -8,3 +8,21 @@ print('"3"')
 print(3)
 print(3,'ss')
 print(3+'ss')
+
+
+'''
+Output：
+11
+3s
+3s
+ss s
+sss
+'3'
+"3"
+3
+3 ss
+Traceback (most recent call last):
+  File "print.py", line 10, in <module>
+    print(3+'ss')
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+'''
